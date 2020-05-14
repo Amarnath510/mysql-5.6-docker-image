8bee895a845f368acd9cb1d39e024bd9e7369123
camarnath <camarnath@castlighthealth.com>
2020-05-14 16:00:18 +0530

    updated sql script

--- company.sql
+++ company.sql
@@ -9,8 +9,3 @@ CREATE TABLE `employees` (
   UNIQUE KEY `ix_email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='Table contains all employee details.';
 
-INSERT INTO employees(first_name, last_name, email) VALUES('amarnath1', 'chandana1', 'amarnath1@github.com');
-INSERT INTO employees(first_name, last_name, email) VALUES('amarnath2', 'chandana2', 'amarnath2@github.com');
-INSERT INTO employees(first_name, last_name, email) VALUES('amarnath3', 'chandana3', 'amarnath3@github.com');
-INSERT INTO employees(first_name, last_name, email) VALUES('amarnath4', 'chandana4', 'amarnath4@github.com');
-INSERT INTO employees(first_name, last_name, email) VALUES('amarnath5', 'chandana5', 'amarnath5@github.com');
